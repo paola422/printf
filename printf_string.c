e7a02dc7f13dabf0967173e03ade6ad0495c885b
paola422 <paola.canchala27@gmail.com>
2021-07-28 10:04:14 -0700

    Correction of betty

--- printf_string.c
+++ printf_string.c
@@ -7,14 +7,16 @@
 
 int_printstring(va_list s)
 {
-  char *character;
-  unsigned int i = 0;
-  if(character==NULL)
-    character=NULL;
-  while(characte [i])
-  {
-    _putchar(character [i])
-      i++;
-  }
-  return(i);
+	char *character;
+	unsigned int i = 0;
+
+	if (character == NULL)
+		character = NULL;
+
+	while (character[i])
+	{
+		_putchar(character[i])
+		i++;
+	}
+return (i);
 }
