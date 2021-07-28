efbf93aeb91096691d0ec8353c29cb92d359bb91
root <root@Portatil.localdomain>
2021-07-27 22:40:57 -0500

    function to string

---
+++ printf_string.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * printf_string - function to string
+ * @s: string
+ * Return:int
+ */
+
+int_printstring(va_list s)
+{
+  char *character;
+  unsigned int i = 0;
+  if(character==NULL)
+    character=NULL;
+  while(characte [i])
+  {
+    _putchar(character [i])
+      i++;
+  }
+  return(i);
+}
